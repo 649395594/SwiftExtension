ea8ca076d82c91f43c27d42c49da0b65d9646837
张星宇 <649395594@qq.com>
2016-01-17 15:00:09 +0800

    新增字符串分割函数

--- Swift-轮子/String+Operation.swift
+++ Swift-轮子/String+Operation.swift
@@ -9,11 +9,28 @@
 import Foundation
 
 extension String{
-    func Reverse() -> String{
+    /**
+     逆序输出String
+     
+     :returns: 反顺序的字符串，比如"abc".reverse()输出结果为"cba"
+     */
+    func kt_reverse() -> String{
         var returnString: String = ""
-        for (var i = count(self) - 1; i >= 0; --i) {
-            returnString.append(self[advance(self.startIndex, i)])
+        for (var i = self.characters.count - 1; i >= 0; --i) {
+            returnString.append(self[self.startIndex.advancedBy(i)])
         }
         return returnString
+    }
+    
+    /**
+     根据某个字符分割字符串
+     "a,b,c".kt_split(",") = ["a", "b", "c"]
+     
+     :param: seperator 分隔符
+     
+     :returns: 返回一个数组，数组中的元素是字符串
+     */
+    func kt_split(seperator: Character) -> Array<String> {
+        return characters.split{ $0 == seperator}.map { String($0) }
     }
 }
